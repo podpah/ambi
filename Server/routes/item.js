d6056d14dbaf77dec91ccda31478e831dfbcb74f
podpah <pedrinhohrodrigues@outlook.com>
2022-09-01 12:39:58 +0100

    Full page and single page view completed, moving branches

--- Server/routes/item.js
+++ Server/routes/item.js
@@ -7,10 +7,11 @@ app.use(cors())
 
 app.get('/' , async (req, res) => {
     let quer = await Item.findAll()
-    let {title, description,price,category,image } = quer
+    let {id,title, description,price,category,image } = quer
     
     let xy = quer.map((item) => {
         return load = {
+            id : item.id,
             title: item.title,
             description : item.description,
             image : item.image,
@@ -25,10 +26,12 @@ app.get('/' , async (req, res) => {
 
 app.get('/:item', async (req,res) => {
 
+        // if (req.params.item.length <= 2 ) {
         let quer = await Item.findOne({where: {id:req.params.item}})
         if (quer != null) {
-            let {title, description,price,category,image } = quer
+            let {id, title, description,price,category,image } = quer
             let load = {
+                id : id,
                 title: title,
                 description : description,
                 image : image,
@@ -36,8 +39,24 @@ app.get('/:item', async (req,res) => {
                 category : category
             }
             res.send(load)}
+        // }
+        // else {
+            // console.log(req.params.item)
+            // let quer = await Item.findOne({where: {title:req.params.item}})
+            // if (quer != null) {
+                // let {title, description,price,category,image } = quer
+                // let load = {
+                    // title: title,
+                    // description : description,
+                    // image : image,
+                    // price : price,
+                    // category : category
+                // }
+                // res.send(load)}
+        // }
 })
 
 app.listen(1234, () => {
     console.log('The server is live and listening at http://localhost:1234')
 })
+ 
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 
 function App() {
 
-  const [view , setView] = useState("list")
+  const [view , setView] = useState(true)
   const [inv, setInv] = useState([])
   const [page, setPage] = useState({})
 
@@ -11,7 +11,13 @@ function App() {
     const res = await fetch('http://localhost:1234')
     const data = await res.json()
     setInv(data)
-    console.log(inv)
+  }
+
+  async function single(x) {
+    setView(!view)
+    const resd = await fetch('http://localhost:1234/')
+    const dataa = await resd.json()
+    setPage(x)
   }
 
   useEffect( () => {
@@ -19,11 +25,31 @@ function App() {
   } ,[])
 
   return <>
-          {inv.map((x) => {return <div className="items">
+
+  {view ?
+
+          inv.map((x) => {return <div className="items" onClick={() => {single(x)}}>
           <p>{x.title}</p>
           <img src={x.image} alt={x.title} />
           <p>Price: £{x.price}</p>
-          </div> } )}
+          </div> } )
+      
+      :
+
+      <>
+
+
+    <div className="singlitem">
+      <p>{page.title}</p>
+      <img src={page.image} alt={page.title} />
+      <p>{page.description}</p>
+      <p>£{page.price}</p>
+      <p>Category: {page.category}</p>
+      <button onClick={() => setView(!view)}>Back</button>
+      </div>
+      </>
+      }
+
     </>;
 }
 
